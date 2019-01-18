0dfa1c007f7cd7c1695e82fe0fd621d42ccf00ba
Billy Witherspoon <wwither@gmail.com>
2019-01-18 04:27:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,12 @@ def batch_badge_creator(namesArray)
   end
   return messageArray
 end
+
+def assign_rooms(namesArray)
+  roomsArray = []
+  i = 0
+  until i == 7
+    roomsArray << "Hello, #{namesArray[i]}! You'll be assigned to room #{i + 1}!"
+  end
+  return roomsArray
+end
