dd0249c895defd218b67122d9fc43aa3e81540d3
Billy Witherspoon <wwither@gmail.com>
2019-01-18 04:18:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,12 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(namesArray)
+  messageArray = []
+  namesArray.each do |name|
+    messageArray.push(badge_maker(name))
+  end
+end
+    
+  
